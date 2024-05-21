a6a7627e69afe1e3c3add2d23a6543e3c9e678e5
Ted Collins <theodore.collins2023@outlook.com>
2024-05-21 11:08:49 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Assignment3._2._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix1 = { { 1, 2 }, { 3, 4 } };
+            int[,] matrix2 = { { 5, 6 }, { 7, 8 } };
+            int[,] matrix3 = new int[2,2];
+
+            AddMatrix(matrix1, matrix2, matrix3);
+            PrintMatices(matrix1);
+            PrintMatices(matrix2);
+            PrintMatices(matrix3);
+
+        }
+        
+        public static void AddMatrix(int[,] m1, int[,] m2, int[,] m3)
+        {
+            int rows = m1.GetLength(0);
+            int columns = m1.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    m3[i, j] = m1[i, j] + m2[i,j];
+                }
+            }
+        }
+
+        public static void PrintMatices(int[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{matrix[i, j]}|");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}
